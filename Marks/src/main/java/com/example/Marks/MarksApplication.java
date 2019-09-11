3ab7a3cc83233bf20026de46c6711952a92b337d
t-paudel <tushar.paudel@sysbiz.io>
2019-09-11 12:31:47 +0530

    added one server and two clients

---
+++ Marks/src/main/java/com/example/Marks/MarksApplication.java
@@ -0,0 +1,13 @@
+package com.example.Marks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksApplication.class, args);
+	}
+
+}
